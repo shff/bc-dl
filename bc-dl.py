7c540addf7c9d96ab85ee96b4ff3b58900a9f42c
SHF <25377830+shff@users.noreply.github.com>
2019-10-15 15:24:54 -0300

    Update to Python3

--- bc-dl.py
+++ bc-dl.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import sys
 import re
 import json
-import urllib2
-import urlparse
+import ssl
+import urllib.request, urllib.error, urllib.parse
+import urllib.parse
 import tempfile
 import subprocess
-import HTMLParser
+import html
+import html.parser
 
 import eyed3
 from eyed3.id3.frames import ImageFrame
@@ -22,67 +24,78 @@ def read_js_object(codes):
   return json.loads(code)
 
 try:
-  url = sys.argv[1]
-  directory = "~/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized"
+  ctx = ssl.create_default_context()
+  ctx.check_hostname = False
+  ctx.verify_mode = ssl.CERT_NONE
 
-  content = urllib2.urlopen(url).read().decode('utf-8')
+  url = sys.argv[1]
+  directory = "~/Downloads"
+
+  content = urllib.request.urlopen(url, context=ctx).read().decode('utf-8')
   tracks = read_js_object(content.split("var TralbumData = ")[1].split("};")[0] + "}")
   art = content.split('<link rel="image_src" href="')[1].split('">')[0]
 
   tmpdir = tempfile.mkdtemp()
-  html = HTMLParser.HTMLParser()
 
-  print "Downloading cover art"
-  cover_art_obj = urllib2.urlopen(art)
+  print("Downloading cover art")
+  cover_art_obj = urllib.request.urlopen(art, context=ctx)
   cover_art = cover_art_obj.read()
 
+  artist = tracks['artist']
+  artist_url = tracks['url']
+  album_title = tracks['current']['title'] if 'current' in tracks else tracks['artist']
+  album_year = tracks['album_release_date'].split()[2] if 'album_release_date' in tracks and tracks['album_release_date'] else tracks['current']['new_date'].split()[2]
+  track_count = len(tracks['trackinfo'])
+
   for track in tracks['trackinfo']:
-    if track['file'] and not (isinstance(track['file'], unicode) or isinstance(track['file'], str)):
-      if 'mp3-128' in track['file']:
-        artist = tracks['artist']
-        track_num = track['track_num'] or 1
-        track_count = len(tracks['trackinfo'])
-        song_title = track['title'].replace(" (remastered)", "")
-        album_title = tracks['current']['title'] if 'current' in tracks else tracks['artist']
-        album_year = tracks['album_release_date'].split()[2] if 'album_release_date' in tracks and tracks['album_release_date'] else tracks['current']['new_date'].split()[2]
-        url = track['file']['mp3-128']
+    track_num = track['track_num'] or 1
+    song_title = track['title'].replace(" (remastered)", "")
 
-        filename = u"%s/%02d - %s.mp3" % (directory, track_num, song_title.replace("/", " - "))
-        filename = os.path.expanduser(filename)
-        tmp_file = tmpdir + "/track%d.mp3" % track_num
+    if not track['file'] or 'mp3-128' not in track['file']:
+      continue
 
-        try:
-          lyrics = html.unescape(re.sub('^[\t\n ]+', '', re.sub('<br>', '', content.split('<dd id="_lyrics_%i">' % track_num)[1].split('</dd>')[0]))).encode('utf-8')
-        except:
-          lyrics = False
+    url = track['file']['mp3-128']
 
-        if url.startswith('//'):
-          url = 'http:' + url
+    filename = "%s/%02d - %s.mp3" % (directory, track_num, song_title.replace("/", " - "))
+    filename = os.path.expanduser(filename)
+    tmp_file = tmpdir + "/track%d.mp3" % track_num
 
-        print u'Downloading %02d of %02d: %s' % (track_num, track_count, song_title)
+    try:
+      lyrics = content.split('<dd id="_lyrics_%i">' % track_num)[1].split('</dd>')[0]
+      lyrics = re.sub('<br>', '', lyrics)
+      lyrics = re.sub('^[\t\n ]+', '', lyrics)
+      lyrics = html.unescape(lyrics).encode('utf-8')
+    except:
+      lyrics = False
 
-        f = urllib2.urlopen(url)
-        with open(tmp_file, "wb") as local_file:
-          local_file.write(f.read())
+    if url.startswith('//'):
+      url = 'http:' + url
 
-        audiofile = eyed3.load(tmp_file)
-        audiofile.tag = eyed3.id3.Tag()
-        audiofile.tag.artist = artist.decode('utf-8')
-        audiofile.tag.album = album_title.decode('utf-8')
-        audiofile.tag.album_artist = artist.decode('utf-8')
-        audiofile.tag.title = song_title.decode('utf-8')
-        audiofile.tag.track_num = (track_num, track_count)
-        audiofile.tag.disc_num = (1, 1)
-        audiofile.tag.recording_date = album_year
-        if lyrics:
-          audiofile.tag.lyrics.set(lyrics.decode('utf-8'))
-        audiofile.tag.images.set(ImageFrame.FRONT_COVER, cover_art, u'image/jpeg')
-        audiofile.tag.save()
+    print('Downloading %02d of %02d: %s' % (track_num, track_count, song_title))
 
-        os.rename(tmp_file, filename.encode('utf-8'))
+    f = urllib.request.urlopen(url, context=ctx)
+    with open(tmp_file, "wb") as local_file:
+      local_file.write(f.read())
+
+    audiofile = eyed3.load(tmp_file)
+    audiofile.tag = eyed3.id3.Tag()
+    audiofile.tag.artist = artist
+    audiofile.tag.album = album_title
+    audiofile.tag.album_artist = artist
+    audiofile.tag.title = song_title
+    audiofile.tag.track_num = (track_num, track_count)
+    audiofile.tag.disc_num = (1, 1)
+    audiofile.tag.recording_date = album_year
+    audiofile.tag.comments.set(artist_url)
+    if lyrics:
+      audiofile.tag.lyrics.set(lyrics.decode('utf-8'))
+    audiofile.tag.images.set(ImageFrame.FRONT_COVER, cover_art, 'image/jpeg')
+    audiofile.tag.save()
+
+    os.rename(tmp_file, filename.encode('utf-8'))
 except IndexError as e:
-  print "Invalid Bandcamp data. Either the URL is correct or their website changed."
-except urllib2.URLError as e:
-  print "Download error: " + str(e.reason)
+  print("Invalid Bandcamp data. Either the URL is correct or their website changed.")
+except urllib.error.URLError as e:
+  print("Download error: " + str(e.reason))
 except KeyboardInterrupt as e:
   pass
